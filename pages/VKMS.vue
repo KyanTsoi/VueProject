6c9cc1468c006c757604016b7bafead3c045b759
KyanTsoi <kyantsoi1@gmail.com>
2025-09-26 14:31:48 +0200

    Initial commit

---
+++ pages/VKMS.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>overzicht pagina</h1>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>Welkom op de homepage</h1>
+    <p>
+      Dit is de hoofdpagina van de app. Je kunt vanaf hier linken naar andere pagina's.
+    </p>
+  </div>
+</template>
